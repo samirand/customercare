62d169433747d143bc779696993d6e29905da284
samirand <samiranbims@gmail.com>
2019-08-23 08:06:23 -0400

    2nd version of code

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ class HelloWorld
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello, World"); 
+        System.out.println("Hello, World");
+	System.out.println("This is the 2nd version"); 
     } 
 } 
