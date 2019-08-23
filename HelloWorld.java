7ca6fddc597ccb0208a0c3c1bfa3de3b1825d495
samirand <samiranbims@gmail.com>
2019-08-23 08:58:20 -0400

    New changes on code

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,5 +7,6 @@ class HelloWorld
         System.out.println("Hello, World");
 	System.out.println("This is the 2nd version"); 
 	System.out.println("This is the 3rd version");
+	System.out.println("Some changes has made on this code");
     }
 } 
